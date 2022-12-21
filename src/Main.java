47cac0731465c2e7f4c1e676bc332428e2d4b37a
Sebastian Miron <sebastian.miron01@e-uvt.ro>
2022-12-21 11:46:08 +0200

    commit 5

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
 
-public class Main {
-    public static void main(String[] args)
-    {
-        Game g=new Game(true);
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.application.Application.launch;
+
+class ChessBoard extends Application {
+    public void start(Stage primaryStage) throws IOException {
+        Button ok = new Button("OK");
+        Scene scene = new Scene(ok, 320, 240);
+        primaryStage.setTitle("Hello!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        Stage stage = new Stage();
+        Game g = new Game(true);
         g.gameStart();
+    }
+
+
+    public static void main(String[] args) {
+        launch();
     }
 }
